fd0f15c8ff67323682a6ce502c4c8608f7f387b8
Zach Aysan <zachaysan@gmail.com>
2015-01-26 16:09:49 -0500

    Move to config file instead of flags, it will make testing easier

--- fhp.go
+++ fhp.go
@@ -1,17 +1,47 @@
-package main
+package fhp
 
-import "flag"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
 
-var consumerKeyPtr = flag.String("ckey", "", "Your 500px consumer key")
+	"github.com/garyburd/go-oauth/oauth"
+)
 
-func init() {
-	flag.StringVar(consumerKeyPtr, "c", "bar", "a string var")
+var oauthClient = oauth.Client{
+	TemporaryCredentialRequestURI: "https://api.500px.com/v1/oauth/request_token",
+	ResourceOwnerAuthorizationURI: "https://api.500px.com/v1/oauth/authorize",
+	TokenRequestURI:               "https://api.500px.com/v1/oauth/access_token",
 }
 
-func main() {
-	flag.Parse()
-	consumerKey := *consumerKeyPtr
+type Configuration struct {
+	ConsumerKey    string
+	ConsumerSecret string
+}
 
+var configuration Configuration
+
+func init() {
+	filename := "config.json"
+	file, err := os.Open(filename)
+	decoder := json.NewDecoder(file)
+
+	if err != nil {
+		fmt.Println("error:", err)
+		println("Problem loading", filename, "aborting")
+		return
+	}
+
+	configuration = Configuration{}
+	err = decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("error:", err)
+		println("Malformed", filename, "aborting")
+		return
+	}
+}
+
+func Photo(consumerKey string) {
+	println(configuration.ConsumerKey)
 	println(consumerKey)
-	println("done")
 }
