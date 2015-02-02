67206227f13c680df203193ef6a1474322877278
Zach Aysan <zachaysan@gmail.com>
2015-02-01 22:47:13 -0500

    Create SearchPhotosByTerm

--- photos.go
+++ photos.go
@@ -5,7 +5,9 @@ import (
 	"strconv"
 )
 
-func (fhpApi *FhpApi) GetPhoto(photo_id int, values url.Values) (photoResp PhotoResp, err error) {
+func (fhpApi *FhpApi) GetPhoto(photo_id int,
+	values url.Values) (photoResp PhotoResp, err error) {
+
 	response_ch := make(chan response)
 
 	fhpApi.queryQueue <- query{BaseUrl + "/v1/photos/" + strconv.Itoa(photo_id),
@@ -15,5 +17,26 @@ func (fhpApi *FhpApi) GetPhoto(photo_id int, values url.Values) (photoResp Photo
 		response_ch}
 
 	return photoResp, (<-response_ch).err
+}
 
+func (fhpApi *FhpApi) SearchPhotosByTerm(term string,
+	values url.Values) (photoSearchResp PhotoSearchResp, err error) {
+
+	values.Set("term", term)
+
+	return searchPhotos(fhpApi, values)
+}
+
+func searchPhotos(fhpApi *FhpApi,
+	values url.Values) (photoSearchResp PhotoSearchResp, err error) {
+
+	response_ch := make(chan response)
+
+	fhpApi.queryQueue <- query{BaseUrl + "/v1/photos/search",
+		values,
+		&photoSearchResp,
+		HTTP_GET,
+		response_ch}
+
+	return photoSearchResp, (<-response_ch).err
 }
